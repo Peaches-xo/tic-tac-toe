39ea71bccdb20a8d50ab9fba9d2735909e2b5695
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-08-09 17:54:12 +1000

    updated

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gameBoard = document.querySelector(".gameBoard");
 
+
 const player1 = new Player(true);
 const player2 = new Player(false);
+
 
 
 gameBoard.addEventListener('click', ()=>{
@@ -21,6 +23,91 @@ if (player1.turn){
 });
 
 
+// var nav = document.querySelector('.nav');
+// var toggleNav = function (){
+//     var that = this;
+//     console.log(that);
+//     setTimeout(function(){
+//         console.log(that);
+//     }, 1000);
+// };
+// nav.addEventListener('click', toggleNav, false);
+
+//  var links = document.querySelectorAll('nav li');
+//  for (var i = 0; i < links.length; i++){
+//     (function (){
+//         console.log(this);
+//     }).call(links[i]);
+// };
+
+
+//     var myFunction = function (){
+//         console.log("myFunction has run")
+
+//     };
+
+
+// myFunction();
+
+
+//define module
+var Module = (function () {
+
+       
+
+        var _privateMethod = function() {
+            console.log("privateMethod called");
+   
+        };
+        var publicMethod = function(){
+            _privateMethod();
+            console.log("publicMethod called");
+       
+        };
+        var anotherPublicMethod = function(){
+            console.log("anotherPublicMethod called");
+           
+        };
+
+        return {
+            publicMethod: publicMethod,
+            anotherPublicMethod: anotherPublicMethod
+        };
+
+
+    // return {
+    //     publicMethod: function (){
+    //         privateMethod();
+    //         console.log("publicMethod");
+    //     // myMethod: function() {
+    //     //     console.log('myMethod has been called.');
+    //     // }, 
+    //     // someOtherMethod: function(){
+    //     //     console.log('someotherMethod has been called.');
+    //     // }
+    //     }
+    // };
+})();
+
+Module.publicMethod();
+// Module.privateMethod();
+
+// const car = {};
+
+// (function(){
+//     var start = () => {
+//         console.log('start');
+//     };
+
+//     const stop = () =>{
+//         console.log('stop');
+//     };
+
+// })();
+
+
+// start();
+// stop();
 
 function Player (turn) {
 	this.turn = turn;
