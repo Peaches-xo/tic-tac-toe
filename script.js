b3c6331c1295f9445e6a7c1d74072edd2f1bb237
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-08-14 16:55:40 +1000

    updated UI

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const gameBoard = document.querySelector(".gameBoard");
 
-
 const player1 = new Player(true);
 const player2 = new Player(false);
-
-
 
 gameBoard.addEventListener('click', ()=>{
 
@@ -17,43 +14,16 @@ if (player1.turn){
     player1.toggleTurn();
     player2.toggleTurn();
 }
-    
+    });
 
 
-});
-
-
-// var nav = document.querySelector('.nav');
-// var toggleNav = function (){
-//     var that = this;
-//     console.log(that);
-//     setTimeout(function(){
-//         console.log(that);
-//     }, 1000);
-// };
-// nav.addEventListener('click', toggleNav, false);
-
-//  var links = document.querySelectorAll('nav li');
-//  for (var i = 0; i < links.length; i++){
-//     (function (){
-//         console.log(this);
-//     }).call(links[i]);
-// };
-
-
-//     var myFunction = function (){
-//         console.log("myFunction has run")
-
-//     };
-
-
-// myFunction();
 
 
 //define module
-var Module = (function () {
+var GameBoard = (function () {
 
-       
+       //needs to contain gameboard array
+       let gameboardArray = [];
 
         var _privateMethod = function() {
             console.log("privateMethod called");
@@ -73,54 +43,6 @@ var Module = (function () {
             publicMethod: publicMethod,
             anotherPublicMethod: anotherPublicMethod
         };
+    })();
 
 
-    // return {
-    //     publicMethod: function (){
-    //         privateMethod();
-    //         console.log("publicMethod");
-    //     // myMethod: function() {
-    //     //     console.log('myMethod has been called.');
-    //     // }, 
-    //     // someOtherMethod: function(){
-    //     //     console.log('someotherMethod has been called.');
-    //     // }
-    //     }
-    // };
-})();
-
-Module.publicMethod();
-// Module.privateMethod();
-
-// const car = {};
-
-// (function(){
-//     var start = () => {
-//         console.log('start');
-//     };
-
-//     const stop = () =>{
-//         console.log('stop');
-//     };
-
-// })();
-
-
-// start();
-// stop();
-
-function Player (turn) {
-	this.turn = turn;
-}
-
-
-  
-Player.prototype.toggleTurn = function() {
-	this.turn = !this.turn
-}
-
-// player1.toggleTurn()
-// player2.toggleTurn()
-  
-// player1.turn // true
-// player2.turn // false
